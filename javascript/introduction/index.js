85c114a53cf32c2f9b4919e3069d55d2e73117fd
NaimAbdullah <nabdullah0389@gmail.com>
2023-05-01 20:44:51 -0500

    added javascript

---
+++ javascript/introduction/index.js
@@ -0,0 +1,63 @@
+// Console
+console.log("Log coming from index.js");
+// console.warn("Warning");
+// console.error("Error");
+
+// Activity
+console.log(4 + 3);
+document.write("Hello from index.js");
+document.write(4 + 3);
+// alert("Are you ready?")
+
+// Variables
+// int a = 10;
+
+var a = 10;
+var b = "this is String";
+
+console.log(a + b);
+
+// Avoid reserved words
+
+// var char = "break";
+// var break = "break";
+
+var age = 20;
+var year = 2000;
+
+console.log("My age is " + age + " and I was born in " + year);
+
+// Numbers
+
+// float
+// double
+// int
+
+var num1 = 10;
+var num2 = -50;
+var num2 = 300.45;
+
+console.log(num1);
+console.log();
+console.log();
+console.log();
+console.log();
+
+// If else if else
+
+var isTrue = false;
+
+if (isTrue) {
+    console.log("Its true");
+} else if (!isTrue) {
+    console.log("It's false");
+}
+else {
+    console.log("its something else");
+}
+
+var max = 1;
+// while (max <= 3) {
+//     console.log("while loop");
+//     max++;
+// }
